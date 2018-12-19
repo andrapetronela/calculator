5b3fad67069019d7a1a77fbee78cc74a17507ce0
Andra <andra.popa1989@gmail.com>
2018-12-19 21:22:38 +0000

    add js version

--- script.js
+++ script.js
@@ -1,49 +1,37 @@
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div id="container">
-                <Calculator />
-            </div>
-        )
-    }
-}
 
-class Calculator extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div id="calculator">
-                <div id="display">
-                    Result
-                </div>
-                <div id="buttons">
-                    <div id="clear" className="button">AC</div>
-                    <div id="equals" className="button">=</div>
-                    <div id="zero" className="button">0</div>
-                    <div id="one" className="button">1</div>
-                    <div id="two" className="button">2</div>
-                    <div id="three" className="button">3</div>
-                    <div id="four" className="button">4</div>
-                    <div id="five" className="button">5</div>
-                    <div id="six" className="button">6</div>
-                    <div id="seven" className="button">7</div>
-                    <div id="eight" className="button">8</div>
-                    <div id="nine" className="button">9</div>
-                    <div id="add" className="button operation">+</div>
-                    <div id="subtract" className="button operation">-</div>
-                    <div id="multiply" className="button operation">*</div>
-                    <div id="divide" className="button operation">/</div>
-                    <div id="decimal" className="button">.</div>
-                </div>
-                
-            </div>
-        )
-    }
-}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const calculator = document.querySelector('#buttons');
+const keys = calculator.querySelector('.button');
+
+calculator.addEventListener('click', ev => {
+    if (ev.target.matches('button')) {
+        const key = ev.target;
+        const action = key.dataset.action;
+        if (!action) {
+            console.log('number key');
+        } 
+            if (
+              action === 'add' ||
+              action === 'subtract' ||
+              action === 'multiply' ||
+              action === 'divide'
+            ) {
+              console.log('operator key!')
+            } 
+        if (action === 'decimal') {
+              console.log('decimal key!')
+            }
+            if (action === 'clear') {
+              console.log('clear key!')
+            }
+            if (action === 'equals') {
+              console.log('equal key!')
+            }
+    }
+})
+
+
+
+
+
+
