e78c69dfe6c47271985827a1384b4fa5a81c00a5
Andra <andra.popa1989@gmail.com>
2018-12-19 21:53:15 +0000

    handle number input

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-
-
 const calculator = document.querySelector('#buttons');
-const keys = calculator.querySelector('.button');
+const buttons = calculator.querySelector('.button');
+const display = document.getElementById('display');
+const clear = document.getElementById('clear');
 
 calculator.addEventListener('click', ev => {
     if (ev.target.matches('button')) {
         const key = ev.target;
         const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const number = display.textContent;
+                
         if (!action) {
-            console.log('number key');
-        } 
+            if (number === '0') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = number + keyContent;
+            }
+        } else if (action === 'decimal') {
+            display.textContent = number + '.';
+        }
             if (
               action === 'add' ||
               action === 'subtract' ||
@@ -28,7 +37,14 @@ calculator.addEventListener('click', ev => {
               console.log('equal key!')
             }
     }
+});
+
+clear.addEventListener('click', function() {
+    display.textContent = '0';
 })
+
+
+
 
 
 
