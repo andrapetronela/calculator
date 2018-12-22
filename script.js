f25b60ffcbbbde64691dcba661f8c6b1a690a867
Andra <andra.popa1989@gmail.com>
2018-12-22 21:00:17 +0000

    pass 14/16 tests

--- script.js
+++ script.js
@@ -1,79 +1,96 @@
-let num = '';
-let result = '';
-
 class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            total: '',
-            current: '', 
             display: '0',
+            currentNum: '',
+            sign: '',
+            result: '0',
+            first: '',
+            second: '',
+            lastPressed: '',
+            decimal: false,
         }
     this.handleNumber = this.handleNumber.bind(this);
-    this.handleOperation = this.handleOperation.bind(this);
+
     this.clear = this.clear.bind(this);
     }
     
-    handleNumber(ev) {
-        num += ev.target.value;
+    handleNumber(ev) {   
+        this.state.lastPressed = ev.target.value;
+    
+        this.state.currentNum += ev.target.value;
         this.setState({
-            display: num,
-        });   
+            display: this.state.currentNum,
+        })
+        
+        if (ev.target.name === 'operator') {
+            this.state.first = this.state.currentNum.slice(0, -2);
+            
+        }
+        
+        if (ev.target.value === '=') {
+            let expression = this.state.currentNum.slice(0, -1);
+    
+           this.setState({
+               display: eval(expression),
+               currentNum: eval(expression),
+           })    
+            
+            this.state.second = this.state.currentNum;
+    }
+        
+        console.log('second is ' + this.state.second);
         
         if (this.state.display === '0' && ev.target.value === '0') {
             this.setState({
                 display: '0',
             });
-            num = '';
-        } 
-//        if (this.state.display.includes('.') && ev.target.value === '.') {
-//            console.log('double');
-//        }
-    }
-    
-    handleOperation(ev) {
-        if (ev.target.value === '=') {
-            result = eval(this.state.display);
-            console.log(result);
-            this.setState({
-                display: result,
-            })
-        }         
-    }
+            this.state.currentNum = '';
         
+        }
+       const decimal =  document.getElementById('decimal');    
+        if (this.state.lastPressed === '.') {       
+            if (ev.target.value === '.') {
+                console.log('double');
+            }
+        }        
+}
+    
     clear() {
         this.setState({
+            currentNum: '',
             display: '0',
-            total: '',
-        });
-        num = '';
-    }
+        })
+    }    
       
     render() {
         return (
             <div id="container">
             <div id="calculator">
-                <div id="display">
-                    {this.state.display}
+                
+                <div id="display">{this.state.display}
+                   
                 </div>
+            
                 <div id="buttons">
                     <div id="clear" className="button" onClick={this.clear}>DEL</div>
-                    <input id="equals" className="button" onClick={this.handleOperation} value="=" />
-                    <input id="zero" className="button" onClick={this.handleNumber} value="0" />
-                    <input name="1" id="one" className="button" onClick={this.handleNumber} value="1" />
-                    <input id="two" className="button" onClick={this.handleNumber} value="2" />
-                    <input id="three" className="button" onClick={this.handleNumber} value="3" />
-                    <input id="four" className="button" onClick={this.handleNumber} value="4" />
-                    <input id="five" className="button" onClick={this.handleNumber} value="5" />
-                    <input id="six" className="button" onClick={this.handleNumber} value="6" />
-                    <input id="seven" className="button" onClick={this.handleNumber} value="7" />
-                    <input id="eight" className="button" onClick={this.handleNumber} value="8" />
-                    <input id="nine" className="button" onClick={this.handleNumber} value="9" />
-                    <input id="add" className="button operation" value="+" onClick={this.handleNumber} />
-                    <input id="subtract" className="button operation" value="-" onClick={this.handleNumber} />
-                    <input id="multiply" className="button operation" value="*" onClick={this.handleNumber}/>
-                    <input id="divide" className="button operation" value="/" onClick={this.handleNumber}/>
-                    <input id="decimal" className="button" value="." onClick={this.handleNumber}/>
+                    <input id="equals" className="button" onClick={this.handleNumber} value="=" name="calculate" />
+                    <input id="zero" className="button" onClick={this.handleNumber} value="0" name="digit" />
+                    <input name="1" id="one" className="button" onClick={this.handleNumber} value="1" name="digit" />
+                    <input id="two" className="button" onClick={this.handleNumber} value="2" name="digit" />
+                    <input id="three" className="button" onClick={this.handleNumber} value="3" name="digit" />
+                    <input id="four" className="button" onClick={this.handleNumber} value="4" name="digit" />
+                    <input id="five" className="button" onClick={this.handleNumber} value="5" name="digit" />
+                    <input id="six" className="button" onClick={this.handleNumber} value="6" name="digit" />
+                    <input id="seven" className="button" onClick={this.handleNumber} value="7" name="digit" />
+                    <input id="eight" className="button" onClick={this.handleNumber} value="8" name="digit" />
+                    <input id="nine" className="button" onClick={this.handleNumber} value="9" name="digit" />
+                    <input id="add" className="button operation" value=" + " name="operator" onClick={this.handleNumber} />
+                    <input id="subtract" className="button operation" value=" - " name="operator" onClick={this.handleNumber} />
+                    <input id="multiply" className="button operation" value=" * " name="operator" onClick={this.handleNumber}/>
+                    <input id="divide" className="button operation" value=" / " name="operator" onClick={this.handleNumber}/>
+                    <input id="decimal" className="button" name="decimal" value="." onClick={this.handleNumber}  />
                 </div>
                 
             </div>
